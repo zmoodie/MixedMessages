8983110e56c38a984e799e74d864f94995e530df
zmoodie <zach.moodie@gmail.com>
2021-06-07 16:21:06 -0600

    Created folder structure for project

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Storing messages in an array
+
+let quotes = ['The Best Way To Get Started Is To Quit Talking And Begin Doing.', 'Don’t Let Yesterday Take Up Too Much Of Today.', 'It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.'];
+
+console.log(quotes);
