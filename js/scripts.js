00ca57cb8033636ce6a9b824f97a2b8edbb8ab4b
zmoodie <zach.moodie@gmail.com>
2021-06-07 17:23:17 -0600

    Added styling and quotes to index page

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@
 
 let quotes = ['The Best Way To Get Started Is To Quit Talking And Begin Doing.', 'Don’t Let Yesterday Take Up Too Much Of Today.', 'It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.'];
 
+let authors = ['Harry Potter', 'Bill Einstein', 'Frodolph']
+
 for (let i=0; i < quotes.length; i++) {
-    console.log(quotes[i]);
+    document.getElementById("quote").innerHTML = `<h1>${quotes[i]}</h1>`;
 }
