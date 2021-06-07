d7a21a49a438f112589962da1633824f737bf38e
zmoodie <zach.moodie@gmail.com>
2021-06-07 16:47:23 -0600

    Updated README and got quotes array and for loop added

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,6 @@
 
 let quotes = ['The Best Way To Get Started Is To Quit Talking And Begin Doing.', 'Don’t Let Yesterday Take Up Too Much Of Today.', 'It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.'];
 
-console.log(quotes);
+for (let i=0; i < quotes.length; i++) {
+    console.log(quotes[i]);
+}
